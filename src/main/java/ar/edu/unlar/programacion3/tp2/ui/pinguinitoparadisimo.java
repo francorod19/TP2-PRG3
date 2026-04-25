77d4c999cea93d7451eadac8e1a6ecb3027e8d0b
Franco Rodriguez <francorod2019@gmail.com>
2026-04-25 17:19:56 -0300

    prueba3

--- src/main/java/ar/edu/unlar/programacion3/tp2/ui/pinguinitoparadisimo.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlar.programacion3.tp2.ui;
-
-public class pinguinitoparadisimo {
-    public static void main(String[] args) {
-        System.out.println("¡Bienvenido a Pinguinito Paradisimo!");
-        System.out.println("¡Disfruta de tu aventura en el paraíso de los pingüinos!");
-    }
-
-}
